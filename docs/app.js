2db0d086a175ba52eb42c1550a8d3b5e42fa2cea
Bernhard Urban-Forster <lewurm@gmail.com>
2023-01-23 21:53:58 +0000

    let's see

--- docs/app.js
+++ docs/app.js
@@ -1,3 +1,5 @@
-fetch('https://api.awattar.at/v1/marketdata?start=1561932000000')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+document.addEventListener("DOMContentLoaded", function() {
+  fetch('https://api.awattar.at/v1/marketdata?start=1561932000000')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+});
