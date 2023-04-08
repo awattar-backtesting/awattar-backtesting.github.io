bdfb9f87d98978cc621ef254a905d4c9f49ed3e1
Bernhard Urban-Forster <lewurm@gmail.com>
2023-04-08 19:25:52 +0200

    meh

--- docs/app.js
+++ docs/app.js
@@ -17,7 +17,48 @@ document.addEventListener("DOMContentLoaded", function() {
             Papa.parse(fileContent, {
                 header: true,
                 complete: (results) => {
-                    console.log(results.data);
+                    d = results.data;
+                    console.log("length: " + d.length);
+                    var i = 0;
+                    labels = []
+                    consumption = []
+                    while (i < d.length) {
+                        const {date, usage} = NetzNOE.processEntry(d[i]);
+                        labels.push(date)
+                        consumption.push(usage);
+                        i++;
+                    }
+
+                    var ctx = document.getElementById('awattarChart').getContext('2d');
+
+                    var data = {
+                        // labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                        labels: labels,
+                        datasets: [{
+                            label: 'Electricity Usage',
+                            // data: [150, 200, 180, 220, 250, 230, 240],
+                            data: consumption,
+                            fill: false,
+                            borderColor: 'rgb(75, 192, 192)',
+                            tension: 0.1
+                        }]
+                    };
+
+                    // Define chart options
+                    var options = {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    };
+
+                    // Create chart
+                    var myChart = new Chart(ctx, {
+                        type: 'line',
+                        data: data,
+                        options: options
+                    });
                 }
             });
         };
@@ -26,12 +67,31 @@ document.addEventListener("DOMContentLoaded", function() {
             reader.readAsText(file)
         }
     };
+
+
 });
 
 
-// const submitButton = 
-// $('#submit').click(
-//     function() {
-//         console.log("sup click");
-//     }
-// );
+class Netzbetreiber {
+    name = "name";
+    descriptorUsage = "usage";
+    descriptorTimestamp = "timestamp";
+
+    constructor(name, descriptorUsage, descriptorTimestamp) {
+        this.name = name;
+        this.descriptorUsage = descriptorUsage;
+        this.descriptorTimestamp = descriptorTimestamp;
+    }
+
+    processEntry(entry) {
+        var valueUsage = entry[this.descriptorUsage];
+        var valueTimestamp = entry[this.descriptorTimestamp];
+
+        return {
+            timestamp: valueTimestamp,
+            usage: valueUsage,
+        }
+    }
+};
+
+const NetzNOE = new Netzbetreiber("NetzNÖ", "Gemessene Menge (kWh)", "Messzeitpunkt");
