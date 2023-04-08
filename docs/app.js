f69dcd049118e51a506ecbfe5820ee4dde61a191
Bernhard Urban-Forster <lewurm@gmail.com>
2023-04-08 18:49:13 +0200

    file handling

--- docs/app.js
+++ docs/app.js
@@ -1,24 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
-  fetch('https://api.awattar.at/v1/marketdata?start=1561932000000')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+    fetch('https://api.awattar.at/v1/marketdata?start=1561932000000')
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+
+    console.log("" + document.getElementById('submit'));
+
+    const fileInputs = document.getElementById('file-form');
+
+    fileInputs.onchange = () => {
+        console.log("fileInputs: " + fileInputs[0].files);
+
+        const reader = new FileReader();
+
+        reader.onload = (event) => {
+            const fileContent = event.target.result;
+            Papa.parse(fileContent, {
+                header: true,
+                complete: (results) => {
+                    console.log(results.data);
+                }
+            });
+        };
+
+        for (let file of fileInputs[0].files) {
+            reader.readAsText(file)
+        }
+    };
 });
 
-const fileInputs = document.getElementById('file-form');
-fileInputs.onchange = () => {
-  const reader = new FileReader();
 
-  reader.onload = (event) => {
-    const fileContent = event.target.result;
-    Papa.parse(fileContent, {
-      header: true,
-      complete: (results) => {
-        console.log(results.data);
-      }
-    });
-  };
-
-  for (let file of fileInputs.files) {
-    reader.readAsText(file)
-  }
-};
+// const submitButton = 
+// $('#submit').click(
+//     function() {
+//         console.log("sup click");
+//     }
+// );
