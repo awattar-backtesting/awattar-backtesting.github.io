ea19e6e91e4117115bb49b6e901640f1d45edccd
Bernhard Urban-Forster <lewurm@gmail.com>
2024-04-01 20:37:39 +0200

    add missing import (broke stromnetz graz)

--- docs/netzbetreiber.js
+++ docs/netzbetreiber.js
@@ -1,4 +1,4 @@
-import { parse } from "https://cdn.skypack.dev/date-fns@2.16.1";
+import { parse, parseISO } from "https://cdn.skypack.dev/date-fns@2.16.1";
 
 export class Netzbetreiber {
     name = "name";
