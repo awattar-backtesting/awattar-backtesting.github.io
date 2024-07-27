df6c8778e999f120fda2c3dd9c6d2fd558c0e476
Bernhard Urban-Forster <lewurm@gmail.com>
2024-07-27 23:08:32 +0200

    graalpy bug

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
-# [awattar-backtesting.github.io](https://awattar-backtesting.github.io)
-A tool to visualize your electricity usage with aWATTar
+```sh
+$ python3 --version; python3 build-cache.py
+Python 3.12.4
+dt: 2024-07-27 00:00:00
+unix time stamp: 1722031200000
 
-# Run Locally
-Just start [index.html](docs\index.html), but make sure to allow CORS from local machine.
-Therefore you either run your own webserver or just start chrome with disabled web security:
-`chrome.exe --user-data-dir="C://Chrome dev session" --disable-web-security`
+$ /Users/lewurm/.sdkman/candidates/java/current/bin/python3 --version; /Users/lewurm/.sdkman/candidates/java/current/bin/python3 build-cache.py
+GraalPy 3.10.8 (Oracle GraalVM Native 23.0.0)
+dt: 2024-07-27 00:00:00
+unix time stamp: 1722063600000
+```
--- build-cache.py
+++ build-cache.py
@@ -1,68 +1,14 @@
 import datetime
-import urllib.request
-import urllib.parse
-import pickle
-import os
-import time
 
 def _unix_time_stamp_ms(arg):
-    return str(int(arg.timestamp() * 1000))
+    r = str(int(arg.timestamp() * 1000))
+    print(f"unix time stamp: {r}")
+    return r
 
-class AwattarState:
-    _disk_cache = "awattar_state.pickle"
-    _delay = 6 # seconds
-    def __init__(self):
-        self.addr = "https://api.awattar.at/v1/marketdata"
-        if os.path.isfile(self._disk_cache):
-            self.day_cache = pickle.load(open(self._disk_cache, 'rb'))
-        else:
-            self.day_cache = {}
+def _get_timestamp(day, month, year):
+    dt = datetime.datetime(year = year, month = month, day = day)
+    print(f"dt: {dt}")
+    return _unix_time_stamp_ms(dt)
 
-    def _get_timestamp(self, day, month, year):
-        dt = datetime.datetime(year = year, month = month, day = day, minute = 0)
-        return _unix_time_stamp_ms(dt)
-
-
-    def _fetch_awattar_day(self, day, month, year):
-        if day is None or month is None or year is None:
-            raise Exception("awattar fetch: day/month/year not specified")
-
-        key = (day, month, year)
-        dt_now = datetime.datetime.now()
-        if key in self.day_cache:
-            timestamp, v = self.day_cache[key]
-            if isinstance(v, str):
-                return (timestamp, v)
-
-        timestamp = self._get_timestamp(day, month, year)
-        u = self.addr + "?start=" + timestamp
-        while True:
-            try:
-                with urllib.request.urlopen(u) as url:
-                    jsondump = url.read().decode()
-                    self.day_cache[key] = (timestamp, jsondump)
-                    print(f"requested date={key} from '{u}'")
-                    # print(f"jsondump from '{u}' for key={key} -> {jsondump}")
-                    pickle.dump(self.day_cache, open(self._disk_cache, 'wb'))
-                    return (timestamp, jsondump)
-            except:
-                print("too many requests")
-                time.sleep(self._delay)
-
-
-awattarState = AwattarState()
-
-endDate = dtoday = datetime.datetime.now() - datetime.timedelta(days=1)
-startDate = dtoday - datetime.timedelta(days=7*356)
-
-iDate = startDate
-
-while iDate < endDate:
-    timestamp = awattarState._get_timestamp(iDate.day, iDate.month, iDate.year)
-    cachefile = "./docs/cache/" + str(timestamp)
-    if not os.path.isfile(cachefile):
-        timestamp2, jsondump = awattarState._fetch_awattar_day(iDate.day, iDate.month, iDate.year)
-        assert timestamp == timestamp2, f"iDate: {iDate}, timestamp: {timestamp}, timetamp2: {timestamp2}"
-        with open(cachefile, 'w') as f:
-            f.write(jsondump)
-    iDate = iDate + datetime.timedelta(days=1)
+endDate = datetime.datetime.now()
+_get_timestamp(endDate.day, endDate.month, endDate.year)
