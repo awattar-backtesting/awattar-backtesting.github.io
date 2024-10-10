25f4d1f70a2a866d07c646a9a4a9e7f4dd05bd8f
Bernhard Urban-Forster <lewurm@gmail.com>
2024-10-10 09:47:54 +0200

    build-cache.py: specify timezone

--- build-cache.py
+++ build-cache.py
@@ -4,9 +4,11 @@ import urllib.parse
 import pickle
 import os
 import time
+import pytz
 
 def _unix_time_stamp_ms(arg):
     return str(int(arg.timestamp() * 1000))
+
 
 class AwattarState:
     _disk_cache = "awattar_state.pickle"
@@ -18,23 +20,24 @@ class AwattarState:
         else:
             self.day_cache = {}
 
-    def _get_timestamp(self, day, month, year):
+    def _get_timestamp(self, day, month, year, timezoneinfo):
         dt = datetime.datetime(year = year, month = month, day = day, minute = 0)
+        if timezoneinfo:
+            dt = timezoneinfo.localize(dt)
         return _unix_time_stamp_ms(dt)
 
 
-    def _fetch_awattar_day(self, day, month, year):
+    def _fetch_awattar_day(self, day, month, year, timezoneinfo):
         if day is None or month is None or year is None:
             raise Exception("awattar fetch: day/month/year not specified")
 
         key = (day, month, year)
-        dt_now = datetime.datetime.now()
         if key in self.day_cache:
             timestamp, v = self.day_cache[key]
             if isinstance(v, str):
                 return (timestamp, v)
 
-        timestamp = self._get_timestamp(day, month, year)
+        timestamp = self._get_timestamp(day, month, year, timezoneinfo)
         u = self.addr + "?start=" + timestamp
         while True:
             try:
@@ -50,18 +53,20 @@ class AwattarState:
                 time.sleep(self._delay)
 
 
+cet_timezone = pytz.timezone('Europe/Vienna')
+
 awattarState = AwattarState()
 
-endDate = dtoday = datetime.datetime.now() - datetime.timedelta(days=1)
+endDate = dtoday = cet_timezone.localize(datetime.datetime.now()) - datetime.timedelta(days=1)
 startDate = dtoday - datetime.timedelta(days=7*356)
 
 iDate = startDate
 
 while iDate < endDate:
-    timestamp = awattarState._get_timestamp(iDate.day, iDate.month, iDate.year)
-    cachefile = "./docs/cache/" + str(timestamp)
+    timestamp = awattarState._get_timestamp(iDate.day, iDate.month, iDate.year, cet_timezone)
+    cachefile = "./docs/cache/" + timestamp
     if not os.path.isfile(cachefile):
-        timestamp2, jsondump = awattarState._fetch_awattar_day(iDate.day, iDate.month, iDate.year)
+        timestamp2, jsondump = awattarState._fetch_awattar_day(iDate.day, iDate.month, iDate.year, cet_timezone)
         assert timestamp == timestamp2, f"iDate: {iDate}, timestamp: {timestamp}, timetamp2: {timestamp2}"
         with open(cachefile, 'w') as f:
             f.write(jsondump)
